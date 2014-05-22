f2294f27412dedd76264e5fb02dbe54755f572f3
Taro Watanabe <taro.watanabe@nict.go.jp>
2014-05-23 08:16:58 +0900

    traits for parser implementation

--- rnnp/model_traits.hpp
+++ rnnp/model_traits.hpp
@@ -70,6 +70,30 @@ namespace rnnp
     typedef gradient::Model3 gradient_type;
     typedef parser::Model3   parser_type;
   };
+
+  template <>
+  struct model_traits<parser::Model1>
+  {
+    typedef model::Model1    model_type;
+    typedef gradient::Model1 gradient_type;
+    typedef parser::Model1   parser_type;
+  };
+    
+  template <>
+  struct model_traits<parser::Model2>
+  {
+    typedef model::Model2    model_type;
+    typedef gradient::Model2 gradient_type;
+    typedef parser::Model2   parser_type;
+  };
+    
+  template <>
+  struct model_traits<parser::Model3>
+  {
+    typedef model::Model3    model_type;
+    typedef gradient::Model3 gradient_type;
+    typedef parser::Model3   parser_type;
+  };
 };
 
 
