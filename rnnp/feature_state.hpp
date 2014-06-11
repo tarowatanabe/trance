e5432f37084f3edaed5f71b75fa84c1b272ef033
Taro Watanabe <taro.watanabe@nict.go.jp>
2014-06-11 16:34:50 +0900

    added missed file

---
+++ rnnp/feature_state.hpp
@@ -0,0 +1,73 @@
+// -*- mode: c++ -*-
+//
+//  Copyright(C) 2014 Taro Watanabe <taro.watanabe@nict.go.jp>
+//
+
+#ifndef __RNNP__FEATURE_STATE__HPP__
+#define __RNNP__FEATURE_STATE__HPP__ 1
+
+#include <algorithm>
+
+#include <boost/functional/hash/hash.hpp>
+
+namespace rnnp
+{
+  template <typename Tp>
+  class Allocator;
+
+  class FeatureSet;
+  
+  struct FeatureState
+  {
+    friend class Allocator<FeatureState>;
+    friend class FeatureSet;
+    
+  public:
+    typedef size_t    size_type;
+    typedef ptrdiff_t difference_type;
+    typedef uint32_t  index_type;
+    
+    typedef char* pointer;
+
+    typedef FeatureState state_type;
+    
+  public:
+    FeatureState() : buffer_(0) {}
+    
+  private:
+    FeatureState(pointer buffer) : buffer_(buffer) {}
+    
+  public:
+    bool empty() const { return ! buffer_; }
+    
+    operator bool() const { return ! empty(); }
+    
+  public:
+    
+    friend
+    bool operator==(const state_type& x, const state_type& y)
+    {
+      return x.buffer_ == y.buffer_;
+    }
+    
+    friend
+    bool operator!=(const state_type& x, const state_type& y)
+    {
+      return x.buffer_ != y.buffer_;
+    }
+    
+    friend
+    size_t  hash_value(state_type const& x)
+    {
+      return boost::hash<pointer>()(x.buffer_);
+    }
+    
+  private:
+    pointer buffer_;
+  };
+};
+
+#endif
+
+// include allocator
+#include <rnnp/allocator.hpp>
